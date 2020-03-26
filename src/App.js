f2c8a316a4cc4cc2a711b7b58287ab48f16f6b24
Jethet <henriette.hettinga@gmail.com>
2020-03-26 15:11:04 +0100

    Create components plus routes, create error component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <BrowserRouter></BrowserRouter>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error} exact />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
